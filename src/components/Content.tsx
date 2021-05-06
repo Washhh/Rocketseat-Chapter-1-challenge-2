3433a1cee82be39063e7ab59d40d6d240796f446
Washhh <wiss@cin.ufpe.br>
2021-05-06 19:22:05 -0300

    Desafio Finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
+import React from 'react';
+import Header from './Header';
+import { MovieCard } from './MovieCard';
+
+import { GenreResponseProps, MovieProps } from '../interfaces/interfaces';
+
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className='container'>
+      <Header title={props.selectedGenre.title} />
+      <main>
+        <div className='movies-list'>
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface HeaderProps {
+  title: string;
+}
+
+export default function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className='category'>
+        Categoria:<span> {props.title}</span>
+      </span>
+    </header>
+  );
+}
